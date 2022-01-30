e6cbaf66c07ade153cc122f665ce6b61f10c41c7
Aleksej Kirilishin <a.kirilishin@propellerads.net>
2022-01-30 23:14:22 +0300

    Fetch log file from multiple worker

--- airflow/utils/log/file_task_handler.py
+++ airflow/utils/log/file_task_handler.py
@@ -19,18 +19,17 @@
 import logging
 import os
 from pathlib import Path
-from typing import TYPE_CHECKING, Optional
+from typing import Optional
 
 import httpx
 from itsdangerous import TimedJSONWebSignatureSerializer
 
 from airflow.configuration import AirflowConfigException, conf
+from airflow.models import TaskInstance
 from airflow.utils.context import Context
 from airflow.utils.helpers import parse_template_string, render_template_to_string
 from airflow.utils.log.non_caching_file_handler import NonCachingFileHandler
-
-if TYPE_CHECKING:
-    from airflow.models import TaskInstance
+from airflow.utils.session import create_session
 
 
 class FileTaskHandler(logging.Handler):
@@ -159,26 +158,42 @@ class FileTaskHandler(logging.Handler):
             except Exception as f:
                 log += f'*** Unable to fetch logs from worker pod {ti.hostname} ***\n{str(f)}\n\n'
         else:
+            timeout = None  # No timeout
+            try:
+                timeout = conf.getint('webserver', 'log_fetch_timeout_sec')
+            except (AirflowConfigException, ValueError):
+                pass
+
+            signer = TimedJSONWebSignatureSerializer(
+                secret_key=conf.get('webserver', 'secret_key'),
+                algorithm_name='HS512',
+                expires_in=conf.getint('webserver', 'log_request_clock_grace', fallback=30),
+                # This isn't really a "salt", more of a signing context
+                salt='task-instance-logs',
+            )
+
             url = os.path.join("http://{ti.hostname}:{worker_log_server_port}/log", log_relative_path).format(
                 ti=ti, worker_log_server_port=conf.get('logging', 'WORKER_LOG_SERVER_PORT')
             )
+
+            with create_session() as session:
+                hosts = session.query(TaskInstance.hostname).filter(TaskInstance.hostname != '').distinct()
+
+            for host in hosts:
+                url = os.path.join("http://{host}:{worker_log_server_port}/log", log_relative_path).format(
+                    host=host[0], worker_log_server_port=conf.get('logging', 'WORKER_LOG_SERVER_PORT')
+                )
+
+                response = httpx.head(
+                    url, timeout=timeout, headers={'Authorization': signer.dumps(log_relative_path)}
+                )
+
+                if response.status_code == 200:
+                    break
+
             log += f"*** Log file does not exist: {location}\n"
             log += f"*** Fetching from: {url}\n"
             try:
-                timeout = None  # No timeout
-                try:
-                    timeout = conf.getint('webserver', 'log_fetch_timeout_sec')
-                except (AirflowConfigException, ValueError):
-                    pass
-
-                signer = TimedJSONWebSignatureSerializer(
-                    secret_key=conf.get('webserver', 'secret_key'),
-                    algorithm_name='HS512',
-                    expires_in=conf.getint('webserver', 'log_request_clock_grace', fallback=30),
-                    # This isn't really a "salt", more of a signing context
-                    salt='task-instance-logs',
-                )
-
                 response = httpx.get(
                     url, timeout=timeout, headers={'Authorization': signer.dumps(log_relative_path)}
                 )
